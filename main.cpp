ad28182ac2aaab94289b04e50eeeffa7a1b93181
Roosevelt Jhans Ubaldo Chavez <roosevelt.ubaldo@outlook.com>
2020-11-21 15:29:19 -0500

    almost done

--- main.cpp
+++ main.cpp
@@ -5,31 +5,35 @@
 using namespace std;
 
 
-CImg<char> Binarizar(CImg<float> & img, int umbral)
-{
-    CImg<char> R(img.width(),img.height());
-    for(int i=0;i< img.width();i++)
-        for(int j=0;j< img.height();j++)
-        {
-            int r = img(i,j,0);
-            int g = img(i,j,1);
-            int b = img(i,j,2);
-            if ( (r+g+b)/3 > umbral)
-                R(i,j) = 255;
-            else
-                R(i,j) = 0;
-        }
-    return R;
-}
+// CImg<char> Binarizar(CImg<float> & img, int umbral)
+// {
+//     CImg<char> R(img.width(),img.height());
+//     for(int i=0;i< img.width();i++)
+//         for(int j=0;j< img.height();j++)
+//         {
+//             int r = img(i,j,0);
+//             int g = img(i,j,1);
+//             int b = img(i,j,2);
+//             if ( (r+g+b)/3 > umbral)
+//                 R(i,j) = 255;
+//             else
+//                 R(i,j) = 0;
+//         }
+//     return R;
+// }
 
 
 int main(){
-    CImg<float> A("cara.jpg");
-    CImg<char> R = Binarizar(A,40);
-    //A.display();
-    //R.display();
+    // CImg<float> A("cara.jpg");
+    // CImg<float> B = A.crop(0, 0, 63, 63);
+    // CImg<char> R = Binarizar(B,40);
 
-    quedtree q(R);
+    // B.display();
+    // R.display();
+
+    vector<vector<bool>> values = {{1, 1, 0, 0},{1, 1, 1, 0},{0, 0, 1, 0},{0, 0, 0, 0}};
+
+    quedtree q(values);
     q.insert();
 
     return 1;
--- quadtree.h
+++ quadtree.h
@@ -5,10 +5,10 @@
 #ifndef QUADTREE_QUADTREE_H
 #define QUADTREE_QUADTREE_H
 
-#include "CImg.h"
+//#include "CImg.h"
 
-using namespace cimg_library;
-
+//using namespace cimg_library;
+using namespace std;
 class quedtree{
     struct node{
         int xi, xf, yi, yf;
@@ -29,16 +29,17 @@ class quedtree{
         }
     };
 
-    CImg<char> image;
+    vector<vector<bool>> image;
     node* root;
     int xi,yi;
     int w,h;
 
     bool isColorUnique(int xi, int yi, int xf, int yf){
-        int color = image(xi,yi);
-        for(int i=xi; i<=xf; i++){
-            for(int j=yi; j<=yf; j++){
-                if(color != image(i,j))
+        int color = image[yi][xi];
+        for(int i=xi; i<xf; i++){
+            for(int j=yi; j<yf; j++){
+                cout << i << ' ' << j << ' ' << image[j][i] << ' ' << color << '\n';
+                if(color != image[j][i])
                     return false;
             }
         }
@@ -46,16 +47,22 @@ class quedtree{
     }
 
     node* insert(int xi, int yi, int xf, int yf){
-        if(isColorUnique(xi,yi,xf,yf)){
-            return new node(xi,yi,xf,yf,image((xf-xi)/2,(yf-yi)/2),true);
+        bool unique = isColorUnique(xi,yi,xf,yf);
+        cout << unique << endl;
+        cout << "Inicio: " << xi << " " << yi << endl;
+        cout << "Fin: " << xf << " " << yf << endl;
+        if(unique){
+            return new node(xi,yi,xf,yf,image[(xf+xi)/2][(yf+yi)/2],true);
 
             // guardar el nodo en disco
         } else {
-            node* cur = new node(xi,yi,xf,yi,image((xf-xi)/2,(yf-yi)/2), false);
-            cur->children[0] = insert(xi,yi,xf/2,yf/2);
-            cur->children[1] = insert(xf/2,yi,xf,yf/2);
-            cur->children[2] = insert(xi,yf/2,xf/2,yf);
-            cur->children[3] = insert(xf/2,yf/2,xf,yf);
+            node* cur = new node(xi,yi,xf,yi,-1, false);
+            int mx = (xf+xi)/2;
+            int my = (yf+yi)/2;
+            cur->children[0] = insert(xi,yi,mx,my);
+            cur->children[1] = insert(mx,yi,xf,my);
+            cur->children[2] = insert(xi,my,mx,yf);
+            cur->children[3] = insert(mx,my,xf,yf);
             return cur;
         }
     }
@@ -63,13 +70,13 @@ class quedtree{
 
 public:
 
-    quedtree(CImg<char> image){
+    quedtree(vector<vector<bool>> image){
         root = 0;
         this->image = image;
         this->xi = 0;
         this->yi = 0;
-        this->w = image.width();
-        this->h = image.height();
+        this->w = image[0].size();
+        this->h = image.size();
     }
 
     void insert(){
